a98ed3f3fcd0e516792c107b96cb085daeb873a1
Michele Volonghi <michele.volonghi@gmail.com>
2022-06-19 18:55:11 +0200

    doing

--- micvolo-backend/config/database.js
+++ micvolo-backend/config/database.js
@@ -1,14 +1,26 @@
-const path = require('path');
+const devConnection = (env) => ({
+  client: 'sqlite',
+  connection: {
+    filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  },
+  useNullAsDefault: true,
+  debug: false,
+});
+
+const prodConnection = (env) => ({
+  client: env('DATABASE_CLIENT', 'postgres'),
+  connection: {
+    host: env('DATABASE_HOST', 'localhost'),
+    port: env('DATABASE_PORT', 5432),
+    database: env('DATABASE_NAME', 'develop'),
+    user: env('DATABASE_USERNAME', 'develop'),
+    password: env('DATABASE_PASSWORD', 'develop'),
+  },
+  ssl: {
+    rejectUnauthorized: false
+  },
+});
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT"),
-    connection: {
-      host: env("DATABASE_HOST"),
-      port: env("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-    },
-  },
+  connection: env('NODE_ENV') === 'development' ? devConnection(env) : prodConnection(env)
 });
---
+++ proxy.bat
@@ -0,0 +1,1 @@
+.\cloud_sql_proxy.exe -instances=micvolo-5b784:europe-west8:micvolo=tcp:5432
