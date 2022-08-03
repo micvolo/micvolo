857d553fba01fd3018fdc61bd554e26f7a077d2c
Michele Volonghi <michele.volonghi@gmail.com>
2022-08-03 15:40:02 +0200

    commit

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,54 +1,58 @@
 import Image from "next/image";
 import Link from "next/link";
-import {MenuIcon, XIcon} from "@heroicons/react/outline";
-import {useState} from "react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 import MenuLanguageComponent from "../MenuLanguageComponent";
 
 export default function Navbar() {
     const [menu, setMenu] = useState(false)
+
+    const works = [
+        {
+            name: 'Space',
+            uri: '/space'
+        },
+        {
+            name: 'Macbook',
+            uri: '/macbook'
+        },
+        {
+            name: 'GetBellied',
+            uri: '/getbellied'
+        },
+    ]
     return (
         <nav
             className="flex items-center justify-between flex-wrap bg-black p-1 border-2 border-white rounded-[24px] absolute inset-x-6 top-6 z-[99999]">
             <div className="flex items-center text-white">
                 <Link href="/" passHref>
                     <a className="h-9">
-                        <Image className="rounded-full" width="36" height="36" src={"/images/profile.jpg"} alt="logo"/>
+                        <Image className="rounded-full" width="36" height="36" src={"/images/profile.jpg"} alt="logo" />
                     </a>
                 </Link>
                 <span className="ml-4 text-xl">Michele Volonghi</span>
             </div>
 
             <button onClick={() => setMenu(menu => !menu)} className="sm:hidden text-white">
-                {menu && <XIcon className="h-9"/>}
-                {!menu && <MenuIcon className="h-9"/>}
+                {menu && <XIcon className="h-9" />}
+                {!menu && <MenuIcon className="h-9" />}
             </button>
 
             <div
                 className={`items-center w-auto flex justify-end flex-[0_0_100%] sm:flex-none sm:flex ${!menu && 'hidden'}`}>
                 <div className="text-sm">
-                    {/* <MenuLanguageComponent/> */}
-                    {/*<Link href="/products" passHref>*/}
-                    {/*    <a*/}
-                    {/*        className="inline-block text-white mx-4">*/}
-                    {/*        Ecommerce*/}
-                    {/*    </a>*/}
-                    {/*</Link>*/}
-                    <Link href="/space" passHref>
-                        <a
-                            className="inline-block text-white mx-4">
-                            Space
-                        </a>
-                    </Link>
-                    <Link href="/macbook" passHref>
-                        <a
-                            className="inline-block text-white mx-4">
-                            Macbook
-                        </a>
-                    </Link>
+                    {works.map(work =>
+                        <Link href={work.uri} passHref key={work.name}>
+                            <a
+                                className="inline-block text-white mx-4">
+                                {work.name}
+                            </a>
+                        </Link>
+                    )}
                 </div>
                 <a href="https://stra.studio" target="_blank"
-                   className="text-sm px-4 py-[6px] border-2 rounded-full text-white border-white "
-                   rel="noreferrer">Stra
+                    className="text-sm px-4 py-[6px] border-2 rounded-full text-white border-white "
+                    rel="noreferrer">Stra
                     Studio</a>
             </div>
         </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Poly from "../components/3d/Poly";
 import {Html, OrbitControls} from "@react-three/drei";
 import MarqueeWallpaper from "../components/MarqueeWallpaper";
 import MicVolo from "./micvolo";
+import Head from 'next/head';
 
 const Home: NextPage = () => {
     return (
