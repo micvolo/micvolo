cf159eed6276ede9cdd6e6f8258559705e0743a5
Michele Volonghi <michele.volonghi@gmail.com>
2022-11-06 19:45:14 -0600

    changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout';
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = ({ Component, pageProps }: AppProps) => {
     return (
+        <>
         <Layout>
             <Component {...pageProps} />
         </Layout>
+        <Analytics/>
+        </>
     )
 }
 
