57129864c1f886cbfc6cea2adb48d509a555eac0
Michele Volonghi <michele.volonghi@gmail.com>
2023-11-26 22:06:29 +0100

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import swup from '@swup/astro';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: 'https://micovlo.me',
+    site: 'https://micovlo.com',
     integrations: [
+        sitemap(),
         swup({
             theme: false,
             accessibility: false,
