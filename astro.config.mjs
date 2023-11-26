a8e961f65147fff3975f0c17349b5cc578c273f8
Michele Volonghi <michele.volonghi@gmail.com>
2023-11-26 09:02:55 +0100

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import swup from '@swup/astro';
 
 export default defineConfig({
     site: 'https://micovlo.me',
-    integrations: [swup({
-        theme: false,
-        accessibility: false,
-        containers: ['.panel.right'],
-    })]
+    integrations: [
+        swup({
+            theme: false,
+            accessibility: false,
+            containers: ['.panel.right'],
+        })
+    ]
 });
