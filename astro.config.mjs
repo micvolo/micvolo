ffe6523b5125cfac6a58cc6d6d45fc8e93f46022
Michele Volonghi <michele.volonghi@gmail.com>
2024-02-13 11:47:30 +0100

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
     site: 'https://micvolo.com',
-    integrations: [
-        sitemap(),
-    ],
 });
---
+++ src/content/projects/origami.md
@@ -0,0 +1,7 @@
+---
+title: Origami Project
+year: 2024-01-20
+img: '@assets/projects/origami.webp'
+url: https://origamiproject.it
+tags: Institutional Website
+---
