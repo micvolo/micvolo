80ef6330ece6d0245d3c2dc3ab9853878f5eff53
Michele Volonghi <michele.volonghi@gmail.com>
2026-03-23 09:38:23 +0000

    Clean up wrangler and astro config
    Remove unused flags/bindings, move COOKIE_SECRET to .dev.vars, simplify astro adapter options.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://micvolo.com',
-  adapter: cloudflare({
-    imageService: 'compile',
-    prerenderEnvironment: 'node',
-  }),
+  adapter: cloudflare({ imageService: 'compile' }),
   integrations: [sitemap()],
 });
