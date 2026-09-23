d3c152249a545dd9d0848e92301457131ba9d660
Michele Volonghi <michele.volonghi@gmail.com>
2026-09-23 12:04:45 +0200

    Remove demo client portals from static portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://micvolo.com',
-  adapter: cloudflare({ imageService: 'compile' }),
+  output: 'static',
   integrations: [sitemap()],
 });
